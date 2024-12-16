8960f4d007e05c41e1cb8f7530dfa48d57bd8bb4
Oluwaseye Akintunde <oluwaseye.akintunde@protonmail.com>
2024-12-16 06:27:09 +0100

    c2.5-Method-Based-Routing

--- main.go
+++ main.go
@@ -37,13 +37,18 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form for creating a new snippet"))
 }
 
+// Add a snippetCreatePost handler function
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Save a new snippet..."))
+}
+
 func main() {
-	// Register the two new handler functions and corresponding route patterns with
-	// the servemux, in exactly the same way that we did before.
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view/{id}", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+    // Create the new route, which is restrictd to POST requests only.
+    mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	// Print a log message to say that a server is starting
 	log.Print("starting server on :4000")
